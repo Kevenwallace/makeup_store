dfe46fca7a2e88af673bfc21ff79ed30592fb94a
keven <kevenwallace_@hotmail.com>
2024-07-23 22:15:40 -0300

    feat(models and admin)
    -adiciona models de pedido, pedidoitem e tabelapreço
    -cria os models no admin e ediciona action de criar pedido

--- core/admin.py
+++ core/admin.py
@@ -1,7 +1,40 @@
 from django.contrib import admin
-from core.models import Pedido, PedidoItem, Produto
+from django import forms
+from core.models import Pedido, PedidoItem, Produto, TabelaPreco
 
-# Register your models here.
-admin.site.register(Pedido)
+@admin.action(description="criar pedidos")
+def criar_pedido(modeladmin, request, queryset):
+    pedido_instance = Pedido.objects.create()
+
+    print(pedido_instance)
+    instance = PedidoItem()
+    
+    bulk_creat = []
+    for produtos in queryset:
+        instance = PedidoItem(
+        pedido=pedido_instance,
+        produto=produtos)
+        bulk_creat += [instance]
+    
+    print(bulk_creat)
+    
+    
+
+class ProdutoAdmin(admin.ModelAdmin):
+    list_display = ("no_descricao", "qtd")
+    search_fields = ['no_descricao']  # Campos que serão pesquisáveis
+    actions = [criar_pedido]
+    def qtd_produtos(self, obj):
+        print(obj)
+
+        return obj.qtd
+    
+
+
+
+# Register your models here
+
+admin.site.register(Pedido,)
 admin.site.register(PedidoItem)
-admin.site.register(Produto)
+admin.site.register(Produto, ProdutoAdmin)
+admin.site.register(TabelaPreco)
--- core/models.py
+++ core/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class Produto(models.Model):
@@ -9,10 +10,23 @@ class Produto(models.Model):
     def __str__(self):
         return self.no_descricao
 
+class TabelaPreco(models.Model):
+    produto         = models.ForeignKey(Produto, related_name="precos", on_delete=models.CASCADE)
+    valor_produto   = models.DecimalField(max_digits=10, decimal_places=2, null=False, blank=False)
+    data_inicio     = models.DateField(null=False, blank=False)
+    data_fim        = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.produto.no_descricao} - {self.valor_produto}$"
 
 class Pedido(models.Model):
-    nr_pedido       = models.CharField(max_length=10)
+    nr_pedido       = models.BigAutoField(primary_key=True)
 
 class PedidoItem(models.Model):
-    produto         = models.ForeignKey('Produto', related_name='produto', on_delete=models.CASCADE)
-    valor_venda     = models.CharField(max_length=10, null=False, blank=False)
+    pedido          = models.ForeignKey(Pedido,related_name="pedidos", on_delete=models.CASCADE)
+    produto         = models.ForeignKey(Produto,related_name="produtos", on_delete=models.CASCADE)
+    
+    def __str__(self):
+        preco_atual = self.produto.precos.order_by('-data_inicio').first()
+        return f"{preco_atual}"
+    
